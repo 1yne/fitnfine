5a586c1ee35f4af6824e9c449358d3dc41c6fe83
Caladan8 <tnambiar2008@gmail.com>
2023-02-18 18:57:14 +0530

    Fixed the page description

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,10 +6,11 @@
 
 <svelte:head>
   <title>FITnFINE | Time To Get Serious!</title>
-  <description
-    >Ever had a diet or a workout stashed away in the corner of your brain?
-    Well, that's about to change!</description
-  >
+  <meta
+    name="description"
+    content="Ever had a diet or a workout stashed away in the corner of your brain?
+  Well, that's about to change!"
+  />
 </svelte:head>
 
 <div class="heroSection flex overflow-hidden">
