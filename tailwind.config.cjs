247d6775e01f30a7ee586cf5724c7ec5d1956741
Caladan8 <tnambiar2008@gmail.com>
2024-06-06 22:18:58 +0530

    Modify tailwind config to add Flowbite

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,svelte,ts}"],
+  content: ['./src/**/*.{html,js,svelte,ts}', './node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {
       colors: {
@@ -16,6 +16,18 @@ module.exports = {
         darkBG: "#121212",
         cardBG: "#353536",
         cardBGHover: "#494a4a",
+        primary: {
+          50: '#FFF5F2',
+          100: '#FFF1EE',
+          200: '#FFE4DE',
+          300: '#FFD5CC',
+          400: '#FFBCAD',
+          500: '#FE795D',
+          600: '#EF562F',
+          700: '#EB4F27',
+          800: '#CC4522',
+          900: '#A5371B'
+        }
       },
     },
     screens: {
@@ -33,5 +45,6 @@ module.exports = {
       workoutModalDesktop: "520px",
     },
   },
-  plugins: [],
+  plugins: [require('flowbite/plugin')],
+  darkMode: 'selector',
 };
